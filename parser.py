9ae2afc222a70ceee9974c9c6635829bace498b0
Mayank Sharma <mayank8019@gmail.com>
2020-02-13 17:59:12 +0530

    Added grammars

--- parser.py
+++ parser.py
@@ -26,12 +26,7 @@ def p_TypeDeclaration(p):
 
 def p_ClassOrInterfaceDeclaration(p):
 	'''ClassOrInterfaceDeclaration : ModifierList ClassDeclaration
-								   | ModifierList InterfaceDeclaration '''
-
-def p_ModifierList(p):
-	'''ModifierList : Modifier ModifierList
-					| empty''' 
-
+                                       | ModifierList InterfaceDeclaration '''
 
 def p_ClassDeclaration(p):
 	'''ClassDeclaration : NormalClassDeclaration
@@ -50,10 +45,6 @@ def p_AnnotationTypeDeclaration(p):
 def p_Type(p):
 	''' Type : BasicType Brackets
 			 | ReferenceType Brackets'''
-
-def p_Brackets(p):
-	''' Brackets : LBRACKETS RBRACKETS BRACKETS
-				 | empty'''
 
 def p_BasicType(p):
 	''' BasicType : byte
@@ -116,7 +107,7 @@ def p_ElementValuePairs(p):
                          | ElementValuePair '''
 
 def p_ElementValuePair(p):
-    '''ElementValuePair : IDENTIFIER EQUAL ElementValue '''
+    '''ElementValuePair : Identifier EQUAL ElementValue '''
 
 def p_ElementValue(p):
     '''ElementValue :  Annotation
@@ -140,23 +131,19 @@ def p_ClassBody(p):
 
 def p_ClassBodyDeclaration(p):
     '''ClassBodyDeclaration : SEMICOLON
-                            | Modifiers MemberDecl
+                            | ModifierList MemberDecl
                             | STATIC Block
                             | Block'''
-def p_Modifiers(p):
-    '''Modifiers : Modifier Modifiers
-                 | empty '''
-
 def p_MemberDecl(p):
     '''MemberDecl : MethodOrFieldDecl
-                  |    VOID IDENTIFIER VoidMethodDeclaratorRest
-                  |    IDENTIFIER ConstructorDeclaratorRest
+                  |    VOID Identifier VoidMethodDeclaratorRest
+                  |    Identifier ConstructorDeclaratorRest
                   |    GenericMethodOrConstructorDecl
                   |    ClassDeclaration
                   |    InterfaceDeclaration '''
 
 def p_MethodOrFieldDecl(p):
-    '''MethodOrFieldDecl : Type IDENTIFIER MethodOrFieldRest'''
+    '''MethodOrFieldDecl : Type Identifier MethodOrFieldRest'''
 
 def p_MethodOrFieldRest(p):
     '''MethodOrFieldRest : FieldDeclaratorsRest SEMICOLON
@@ -168,11 +155,273 @@ def p_FieldDeclaratorsRest(p):
 
 # TODO:
 def p_MethodDeclaratorRest(p):
-    '''MethodDeclaratorRest :  FormalParameters '''
+    '''MethodDeclaratorRest : FormalParameters Brackets THROWS QualifiedIdentifierList Block
+                            | FormalParameters Brackets THROWS QualifiedIdentifierList SEMICOLON
+                            | FormalParameters Brackets Block
+                            | FormalParameters Brackets SEMICOLON'''
 
-FormalParameters {[]} [throws QualifiedIdentifierList] (Block | ;)
+def p_VoidMethodDeclaratorRest(p):
+    '''VoidMethodDeclaratorRest : FormalParameters THROWS QualifiedIdentifierList Block
+                                | FormalParameters THROWS QualifiedIdentifierList SEMICOLON
+                                | FormalParameters Block
+                                | FormalParameters SEMICOLON'''
+
+def p_ConstructorDeclaratorRest(p):
+    '''ConstructorDeclaratorRest : FormalParameters THROWS QualifiedIdentifierList Block
+                                 | FormalParameters Block'''
+
+def p_GenericMethodOrConstructorDecl(p):
+    '''GenericMethodOrConstructorDecl : TypeParameters GenericMethodOrConstructorRest'''
+
+def p_GenericMethodOrConstructorRest(p):
+    '''GenericMethodOrConstructorRest : Type Identifier MethodDeclaratorRest
+                                      | VOID Identifier MethodDeclaratorRest
+                                      | Identifier ConstructorDeclaratorRest'''
 
 ################################################################################
+
+# TODO:
+def p_InterfaceBody(p):
+    '''InterfaceBody : LBRACES InterfaceBodyDeclaration RBRACES '''
+
+
+def p_InterfaceBodyDeclaration(p):
+    '''InterfaceBodyDeclaration : SEMICOLON 
+                                | ModifierList InterfaceMemberDecl'''
+
+def p_InterfaceMemberDecl(p):
+    '''InterfaceMemberDecl : InterfaceMethodOrFieldDecl
+                           | VOID Identifier VoidInterfaceMethodDeclaratorRest
+                           | InterfaceGenericMethodDecl
+                           | ClassDeclaration
+                           | InterfaceDeclaration'''
+
+def p_InterfaceMethodOrFieldDecl(p):
+    '''InterfaceMethodOrFieldDecl :  Type Identifier InterfaceMethodOrFieldRest'''
+
+def p_InterfaceMethodOrFieldRest(p):
+    '''InterfaceMethodOrFieldRest : ConstantDeclaratorsRest SEMICOLON
+                                  | InterfaceMethodDeclaratorRest '''
+
+def p_ConstantDeclaratorsRest(p):
+    '''ConstantDeclaratorsRest : ConstantDeclaratorRest COMMA ConstantDeclarator
+                               | ConstantDeclaratorRest'''
+
+def p_ConstantDeclaratorRest(p):
+    '''ConstantDeclaratorRest : Brackets EQUAL VariableInitializer'''
+
+def p_ConstantDeclarator(p):
+    '''ConstantDeclarator :  '''
+    Identifier ConstantDeclaratorRest
+
+def p_InterfaceMethodDeclaratorRest(p):
+    '''InterfaceMethodDeclaratorRest : FormalParameters Brackets THROWS QualifiedIdentifierList SEMICOLON
+                                     | FormalParameters Brackets THROWS QualifiedIdentifierList SEMICOLON'''
+
+def p_VoidInterfaceMethodDeclaratorRest(p):
+    '''VoidInterfaceMethodDeclaratorRest :  FormalParameters THROWS QualifiedIdentifierList SEMICOLON
+                                         | FormalParameters SEMICOLON'''
+
+def p_InterfaceGenericMethodDecl(p):
+    '''InterfaceGenericMethodDecl :  TypeParameters Type Identifier InterfaceMethodDeclaratorRest
+                                  | TypeParameters VOID Identifier InterfaceMethodDeclaratorRest'''
+
+
+def p_ConstantDeclaratorRest(p):
+    '''ConstantDeclaratorRest : Brackets EQUAL VariableInitializer '''
+
+def p_ConstantDeclarator(p):
+    '''ConstantDeclarator :  Identifier ConstantDeclaratorRest'''
+
+def p_InterfaceMethodDeclaratorRest(p):
+    '''InterfaceMethodDeclaratorRest :  FormalParameters Brackets THROWS QualifiedIdentifierList SEMICOLON
+    | FormalParameters Brackets SEMICOLON'''
+
+def p_VoidInterfaceMethodDeclaratorRest(p):
+    '''VoidInterfaceMethodDeclaratorRest :  FormalParameters THROWS QualifiedIdentifierList SEMICOLON
+                                         | FormalParameters SEMICOLON'''
+
+def p_InterfaceGenericMethodDecl(p):
+    '''InterfaceGenericMethodDecl : TypeParameters Type Identifier InterfaceMethodDeclaratorRest
+                                  | TypeParameters VOID Identifier InterfaceMethodDeclaratorRest'''
+
+################################################################################
+
+#TODO:
+def p_FormalParameters(p):
+    '''FormalParameters :  '''
+    #  ( [FormalParameterDecls] )
+
+def p_FormalParameterDecls(p):
+    '''FormalParameterDecls : VariableModifier Type FormalParameterDeclsRest
+                            | Type FormalParameterDeclsRest'''
+
+def p_VariableModifier(p):
+    '''VariableModifier : FINAL
+                        | Annotation '''
+
+def p_FormalParameterDeclsRest(p):
+    '''FormalParameterDeclsRest : VariableDeclaratorId COMMA FormalParameterDecls
+                                | ELLIPSIS VariableDeclaratorId
+                                | VariableDeclaratorId'''
+
+def p_VariableDeclaratorId(p):
+    '''VariableDeclaratorId : Identifier Brackets'''
+
+def p_VariableDeclarators(p):
+    '''VariableDeclarators : VariableDeclarator COMMA VariableDeclarators
+                           | VariableDeclarator'''
+def p_VariableDeclarator(p):
+    '''VariableDeclarator :  Identifier VariableDeclaratorRest'''
+
+# TODO: 
+def p_VariableDeclaratorRest(p):
+    '''VariableDeclaratorRest : Brackets EQUAL VariableInitializer
+                              | Brackets'''
+
+def p_VariableInitializer(p):
+    '''VariableInitializer : ArrayInitializer
+                           | Expression'''
+
+#TODO:
+def p_ArrayInitializer(p):
+    '''ArrayInitializer : LBRACES VariableInitializer MultipleVariableInitializer COMMA RBRACES
+                        | LBRACES VariableInitializer MultipleVariableInitializer RBRACES
+                        | LBRACES VariableInitializer COMMA RBRACES
+                        | LBRACES VariableInitializer RBRACES
+                        | LBRACES RBRACES'''
+
+def p_MultipleVariableInitializer(p):
+    '''MultipleVariableInitializer : MultipleVariableInitializer COMMA VariableInitializer
+                                   | COMMA VariableInitializer'''
+
+################################################################################
+
+#TODO:
+def p_Block(p):
+    '''Block : LBRACES BlockStatements RBRACES'''
+
+def p_BlockStatements(p):
+    '''BlockStatements : LBRACES BlockStatement RBRACES'''
+
+def p_BlockStatement(p):
+    '''BlockStatement : LocalVariableDeclarationStatement
+                      | ClassOrInterfaceDeclaration
+                      | Identifier COLON Statement
+                      | Statement '''
+
+# TODO:
+def p_LocalVariableDeclarationStatement(p):
+    '''LocalVariableDeclarationStatement :  { VariableModifier }  Type VariableDeclarators ;'''
+
+# TODO:
+def p_Statement(p):
+    '''Statement : Block
+                        |    SEMICOLON
+                        |    Identifier COLON Statement
+                        |    StatementExpression SEMICOLON
+                        |    IF ParExpression Statement ELSE Statement
+                        |    IF ParExpression Statement
+                        |    ASSERT Expression [COLON Expression] SEMICOLON
+                        |    ASSERT Expression SEMICOLON
+                        |    SWITCH ParExpression LBRACES SwitchBlockStatementGroups RBRACES
+                        |    WHILE ParExpression Statement
+                        |    DO Statement WHILE ParExpression SEMICOLON
+                        |    FOR LPAREN ForControl RPAREN SEMICOLON Statement
+                        |    break Identifier SEMICOLON
+                        |    break SEMICOLON
+                        |    CONTINUE Identifier SEMICOLON
+                        |    CONTINUE SEMICOLON
+                        |    RETURN Expression SEMICOLON
+                        |    RETURN SEMICOLON
+                        |    throw Expression SEMICOLON
+                        |    SYNCHRONIZED ParExpression Block
+                        |    TRY Block Catches Finally
+                        |    TRY Block Catches
+                        |    TRY Block Finally
+                        |    TRY ResourceSpecification Block Catches Finally
+                        |    TRY ResourceSpecification Block Catches
+                        |    TRY ResourceSpecification Block Finally
+                        |    TRY ResourceSpecification Block
+        '''
+
+def p_StatementExpression(p):
+    '''StatementExpression :  Expression'''
+
+################################################################################ 
+
+def p_Catches(p):
+    '''Catches : CatchClause LBRACES CatchClause RBRACES'''
+
+def p_CatchClause(p):
+    '''CatchClause : CATCH LPAREN VariableModifier CatchType Identifier RPAREN Block
+                   | CATCH LPAREN CatchType Identifier RPAREN Block '''
+
+def p_CatchType(p):
+    '''CatchType :  MultipleQualifiedIdentifier { BOOLEANOR QualifiedIdentifier }'''
+
+def p_MultipleQualifiedIdentifier(p):
+    '''MultipleQualifiedIdentifier : QualifiedIdentifier BOOLEANOR MultipleQualifiedIdentifier
+                                   | QualifiedIdentifier'''
+
+def p_Finally(p):
+    '''Finally :  FINALLY Block'''
+
+def p_ResourceSpecification(p):
+    '''ResourceSpecification : LPAREN Resources COMMA RPAREN
+                             | LPAREN Resources RPAREN '''
+
+def p_Resources(p):
+    '''Resources : Resource SEMICOLON Resources
+                 | Resource'''
+
+def p_Resource(p):
+    '''Resource : VariableModifier ReferenceType VariableDeclaratorId EQUAL Expression
+                | ReferenceType VariableDeclaratorId EQUAL Expression'''
+
+################################################################################ 
+
+# TODO
+def p_SwitchBlockStatementGroups(p):
+    '''SwitchBlockStatementGroups : LBRACES SwitchBlockStatementGroup RBRACES'''
+
+# TODO
+def p_SwitchBlockStatementGroup(p):
+    '''SwitchBlockStatementGroup : SwitchLabels BlockStatements'''
+
+# TODO
+def p_SwitchLabels(p):
+    '''SwitchLabels :  SwitchLabel LBRACES SwitchLabel RBRACES'''
+
+def p_SwitchLabel(p):
+    '''SwitchLabel : CASE Expression COLON
+                   | CASE EnumConstantName COLON
+                   | DEFAULT COLON'''
+
+def p_EnumConstantName(p):
+    '''EnumConstantName : Identifier'''
+
+def p_ForControl(p):
+    '''ForControl : ForVarControl
+                  | ForInit SEMICOLON Expression SEMICOLON ForUpdate
+                  | ForInit SEMICOLON SEMICOLON ForUpdate
+                  | ForInit SEMICOLON Expression SEMICOLON
+                  | ForInit SEMICOLON SEMICOLON '''
+# TODO:
+def p_ForVarControl(p):
+    '''ForVarControl : VariableModifier Type VariableDeclaratorId ForVarControlRest'''
+
+# TODO:
+#  ForVarControlRest:
+    #  ForVariableDeclaratorsRest ; [Expression] ; [ForUpdate]
+    #  : Expression
+
+#  ForVariableDeclaratorsRest:
+    #  [= VariableInitializer] { , VariableDeclarator }
+
+#  ForInit: 
+#  ForUpdate:
+    #  StatementExpression { , StatementExpression }    
 
 #Default Error
 def p_error(p):
@@ -183,6 +432,13 @@ def p_error(p):
 def p_empty(p):
      'empty :'
 
+def p_Brackets(p):
+	''' Brackets : LBRACKETS RBRACKETS BRACKETS
+				 | empty'''
+
+def p_ModifierList(p):
+	'''ModifierList : Modifier ModifierList
+					| empty''' 
 
 
 
