12be35573a88831a078654490eee3a95c77125d9
ankushSsingh <ankushsngh212@gmail.com>
2020-02-13 12:09:50 +0530

    Modified

--- parser.py
+++ parser.py
@@ -3,3 +3,18 @@
 import yacc
 from lexer import lexer,tokens
 
+
+def p_Identifier(p):
+	'''Identifier : IDENTIFIER'''
+
+def p_QualifiedIdentifier(p):
+	'QualifiedIdentifier : Identifier { . Identifier } '
+
+#Default Error
+def p_error(p):
+    print("Input Error")
+    return
+
+
+# Build the parser
+parser = yacc.yacc()
