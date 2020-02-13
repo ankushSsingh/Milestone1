24b518dde260fdbf5eaa0bcafad1adf50947799f
ankushSsingh <ankushsngh212@gmail.com>
2020-02-13 12:25:27 +0530

    modified

--- parser.py
+++ parser.py
@@ -8,7 +8,23 @@ def p_Identifier(p):
 	'''Identifier : IDENTIFIER'''
 
 def p_QualifiedIdentifier(p):
-	'QualifiedIdentifier : Identifier { . Identifier } '
+	'''QualifiedIdentifier : Identifier DOT QualifiedIdentifier 
+						| Identifier'''
+
+def p_QualifiedIdentifierList(p):
+	'''QualifiedIdentifierList : QualifiedIdentifier COMMA QualifiedIdentifierList
+							   | QualifiedIdentifier'''
+def p_TypeDeclaration(p):
+	'''TypeDeclaration : ClassOrInterfaceDeclaration
+					   | SEMICOLON'''
+def p_ClassDeclaration(p):
+	'''ClassDeclaration : NormalClassDeclaration
+						| EnumDeclaration'''
+def p_InterfaceDeclaration(p):
+	''' InterfaceDeclaration: NormalInterfaceDeclaration
+        			| AnnotationTypeDeclaration'''
+
+
 
 #Default Error
 def p_error(p):
