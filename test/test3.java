bf6eaef1cdf2f07708c627e90647ad2d1d490b2d
ankushSsingh <ankushsngh212@gmail.com>
2020-02-24 16:05:17 +0530

    Test Files Added

---
+++ test/test3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Factorial
+{
+public static void main(String args[])
+{
+int n,c, fact = 0;
+System.out.println("Enter a number \n be sure it is positive \n ");
+Scanner sct = new Scanner(System.in);
+n = sct.nextInt();
+if ( n < 0 )
+System.out.println("Number should be non-negative.");
+else
+{
+for ( c = 1 ; c <= n ; c++ )  // Calculating Factorial
+fact = fact*c;
+System.out.println("Factorial of "+n+" is = "+fact);
+}
+}
+}
